b66512c2360d22a855f7e2a0fb272744d3e511dc
Kevin Welton <kevinwe@unity3d.com>
2025-01-09 15:29:15 -0800

    A memcpy is being generated for the sha256() function. memcpy is still not constexpr even in C++ 20, so see if there's another solution.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,51 @@
 #include <stdlib.h>
-#include <string>
+#include <signal.h>
 
 #include "sha256_constexpr/sha256.h"
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// BEGIN Use this section for testing diagnostic output with static assert
+template <sha256_hash_result val>
+struct c_array
+{
+    char data[val.size()] = {};
+
+    constexpr c_array()
+    {
+        std::ranges::copy_n(val.begin(), val.size(), std::begin(data));
+    }
+
+    constexpr const char* c_str() const
+    {
+        return data;
+    }
+};
+
+template <c_array hash_string>
+constexpr bool string_value = false;
+
+template <sha256_hash_result hash>
+void readable_static_assert()
+{
+    constexpr c_array<hash> hashstring;
+    static_assert(string_value<hashstring>);
+}
+// END static assert test infrastructure
+
 int main()
 {
-    constexpr std::string test_value_short = "abcdefgh";
+    constexpr char test_short[] = "abcdefgh";
 
-    constexpr std::string testval_short_sha256 = sha256(test_value_short);
+    constexpr sha256_hash_result test_short_sha256 = sha256(test_short);
 
-    printf("testval_short_sha256\nInitialMessage=%s\nHashedValue=%s\n\n", test_value_short.c_str(), testval_short_sha256.c_str());
+    // Use one (or neither) of the following when testing depending on what you're currently debugging.
+
+    // Cause a compile failure that will print what was in the array
+    //readable_static_assert<test_short_sha256>();
+
+    // Use this one for testing with printf
+    std::array<char, sha256_hash_result_size + 1> stringified = {};
+    memcpy(stringified.data(), test_short_sha256.data(), sha256_hash_result_size);
+    printf("test_short_sha256\nValue=%s\nHash=%s\n\n", test_short, stringified.data());
 
     return 0;
 }
--- sha256_constexpr/sha256.h
+++ sha256_constexpr/sha256.h
@@ -4,15 +4,38 @@
 
 #pragma once
 
-#include <string>
+#include <array>
 
-// temp
-constexpr std::string placeholder(const std::string msg)
+static constexpr size_t sha256_hash_result_size = 256;
+using sha256_hash_result = std::array<char, sha256_hash_result_size>;
+
+template <size_t N, size_t M, size_t... I1, size_t... I2>
+constexpr sha256_hash_result array_concat_impl(
+        const std::array<char, N> first,
+        const std::array<char, M> second,
+        std::index_sequence<I1...>,
+        std::index_sequence<I2...>)
 {
-    return msg + "12345";
+    return { first[I1]..., second[I2]... };
 }
 
-constexpr std::string sha256(const std::string msg)
+template <size_t N, size_t M>
+constexpr sha256_hash_result array_concat(const std::array<char, N> first, const std::array<char, M> second)
 {
-    return placeholder(msg);
+    return array_concat_impl(first, second, std::make_index_sequence<N - 1> {}, std::make_index_sequence<M - 1> {});
+}
+
+
+//temp
+template <size_t N>
+constexpr sha256_hash_result placeholder(const std::array<char, N> msg)
+{
+    constexpr char append_str[] = "12345";
+    return array_concat(msg, std::to_array(append_str));
+}
+
+template <size_t N>
+constexpr sha256_hash_result sha256(const char(&msg)[N])
+{
+    return placeholder(std::to_array(msg));
 }
